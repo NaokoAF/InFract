8b277c84564eb4049d2b3bcc46b488bd42a067f4
Theo <69133598+TheSmallTeaBoi@users.noreply.github.com>
2026-07-01 16:36:50 -0300

    Fix the nix package for the new dependencies. (#2)
    * update gitignore to ignore the dependency fetcher in its new location
    
    * update the udev rules for the new libusb branc and make them optional
    
    * update deps
    
    * Fix systemctl sandbox
    
    * remove extra DeviceAllows
    
    * update README
    
    * Fix wrong name in readme

--- nix/flake.nix
+++ nix/flake.nix
@@ -54,6 +54,12 @@
           options.services.infract = {
             enable = lib.mkEnableOption "InFract controller refractor";
 
+            enableRules = lib.mkOption {
+              type = lib.types.bool;
+              default = true;
+              description = "Enable udev rules for infract controllers";
+            };
+
             package = lib.mkOption {
               type = lib.types.package;
               default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
@@ -100,16 +106,12 @@
               createHome = true;
             };
 
-            services.udev.extraRules = ''
+            services.udev.extraRules = lib.mkIf cfg.enableRules ''
               # UHID access
               KERNEL=="uhid", GROUP="input", MODE="0660", TAG+="uaccess"
 
-              # GameSir HID access
-              SUBSYSTEM=="hidraw", ATTRS{idVendor}=="3537", GROUP="input", MODE="0660", TAG+="uaccess"
-
-              # Hide GameSir Cyclone 2 (XInput, Wired and Wireless)
-              SUBSYSTEM=="usb", ATTRS{idVendor}=="3537", ATTRS{idProduct}=="1053", ATTR{bInterfaceNumber}=="00", GROUP="input", RUN+="/bin/sh -c 'echo -n %k > /sys/bus/usb/drivers/xpad/unbind'"
-              SUBSYSTEM=="usb", ATTRS{idVendor}=="3537", ATTRS{idProduct}=="100b", ATTR{bInterfaceNumber}=="00", GROUP="input", RUN+="/bin/sh -c 'echo -n %k > /sys/bus/usb/drivers/xpad/unbind'"
+              # GameSir access
+              SUBSYSTEM=="usb", ATTRS{idVendor}=="3537", GROUP="input", MODE="0666", TAG+="uaccess"
             '';
 
             systemd.services.infract = {
@@ -131,9 +133,9 @@
 
                 DeviceAllow = [
                   "/dev/uhid rw"
-                  "/dev/uinput rw"
-                  "char-hidraw rw"
                   "char-input rw"
+                  "char-usb_device rw"
+
                 ];
 
                 StateDirectory = "infract";
